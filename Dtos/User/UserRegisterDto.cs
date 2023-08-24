afd590276c77f3640c4b4e2ce083d9afdcf4cb17
Mateusz Migdał <matmigdal@gmail.com>
2023-08-24 22:24:24 +0200

    Created dto for registering

---
+++ Dtos/User/UserRegisterDto.cs
@@ -0,0 +1,7 @@
+﻿namespace dotnet_rpg2.Dtos;
+
+public class UserRegisterDto
+{
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
