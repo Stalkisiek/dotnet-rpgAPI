f5b9981ba3823ece2b3f41a5e7f19858ad41ceb8
Mateusz Migdał <matmigdal@gmail.com>
2023-08-24 20:35:25 +0200

    Created new model (User)

---
+++ Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace dotnet_rpg2.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public byte[] PasswordHash { get; set; } = new byte[0];
+    public byte[] PasswordSalt { get; set; } = new byte[0];
+}
