d0cc1168a8971ce7eccb9d43d2ea34d6b29a2ad2
Mateusz Migdał <matmigdal@gmail.com>
2023-08-26 03:56:31 +0200

    Added data seeding

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,4 +1,5 @@
 ﻿using dotnet_rpg2.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace dotnet_rpg2.Data;
 
@@ -7,6 +8,15 @@ public class DataContext : DbContext
     public DataContext(DbContextOptions<DataContext> options) : base(options)
     {
         
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Skill>().HasData(
+            new Skill { Id = 1, Name = "Fireball", Damage = 30 },
+            new Skill { Id = 2, Name = "Frenzy", Damage = 30 },
+            new Skill { Id = 3, Name = "Snowstorm", Damage = 30 }
+        );
     }
 
     public DbSet<Character> Characters => Set<Character>();
